4a005b80c5ab78f1d893a250427943a9a47a7090
taman.js <tamanwit.25bcs10001@sst.scaler.com>
2026-02-15 21:55:52 +0530

    html merge into server

--- creating server/server.js
+++ creating server/server.js
@@ -1,9 +1,21 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req, res)=>{
     console.log('request made')
+    // set header content type
+    res.setHeader('Content-Type', 'text/html')
+    // rendering html pages
+    fs.readFile('./index.html', 'utf-8', (err, data)=>{
+        if(err){
+            console.log(err)
+            res.end()
+        }
+        res.write(data)
+        res.end()
+    })
 });
 
 server.listen(3000, 'localhost', ()=>{
-    console.log('listening to request')()
+    console.log('listening to request')
 })
